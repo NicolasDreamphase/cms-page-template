16ecd0795c9df4e9a184e371623025870346774e
Nicolas Maas <nicolas.maas@icloud.com>
2019-02-04 15:38:07 +0100

    Centered vertically and horizontally image

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <!-- Meta tags -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="robots" content="noindex, follow">
+
+        <!-- Stylesheets & Google Fonts -->
+        <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+
+        <title>Coming soon - Warehouse 5</title>
+    </head>
+
+    <body>
+        <div class="c-coming-soon">
+            <p class="u-center">
+                <img class="c-coming-soon__img" src="assets/img/coming-soon-warehouse5@3x.png">
+            </p>
+        </div>
+    </body>
+</html>
